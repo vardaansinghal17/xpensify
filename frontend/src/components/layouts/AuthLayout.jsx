58e1270efa1ba4ca6ee0ad4ee3aa116103f08e0e
Vardan Singhal <vardansinghal3808@gmail.com>
2025-10-30 23:02:02 +0530

    fix auth bugs

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -6,7 +6,7 @@ const AuthLayout = ({ children }) => {
     return (
         <div className='flex'>
             <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-                <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+                <h2 className='text-lg font-medium text-black'>Xpensify</h2>
                 {children}
             </div>
 
--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = ({ activeMenu }) => {
         )}
       </button>
 
-      <h2 className="text-lg font-medium text-black">xpensify</h2>
+      <h2 className="text-lg font-medium text-black">Xpensify</h2>
 
       {openSideMenu && (
         <div className="fixed top-[61px] -ml-4 bg-white">
